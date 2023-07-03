38dd11c463409e68abba7d832d07746bc7984e31
Troll321 <atila.ghulwani@gmail.com>
2023-07-03 20:12:23 +0700

    Public HTML compability

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 const setting = {
-    root: __dirname
+    root: process.env.NODE_ENV === "production" ? __dirname : __dirname + "\\public\\"
 };
 
 app.use(express.static("public"));
 
 app.get("*", (req,res)=>{
-    res.sendFile("/html/index.html", setting);
+    res.sendFile("html/index.html", setting);
 });
 
 app.listen(process.env.PORT || 3000);
